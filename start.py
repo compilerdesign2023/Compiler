21a3c924a09e797b9c6042ada52f819fdcd7bdd1
jivtodeprajwal27 <122209058+jivtodeprajwal27@users.noreply.github.com>
2023-01-30 12:19:52 +0530

    Add files via upload
    Added PrintOp which performs print Operation, and multiple number types like fraction (FracLiteral) and integer (IntLiteral).

--- start.py
+++ start.py
@@ -16,7 +16,15 @@ class BoolType:
 class StringType:
     pass
 
-SimType = NumType | BoolType
+@dataclass
+class IntType:
+    pass
+
+@dataclass 
+class FracType:
+    pass
+
+SimType = NumType | BoolType | StringType | IntType | FracType
 
 @dataclass
 class NumLiteral:
@@ -29,19 +37,31 @@ class NumLiteral:
 class BoolLiteral:
     value: bool
     type: SimType =BoolType
-
 @dataclass
 class StringLiteral:
     value: str
     type: SimType=StringType
     
+@dataclass 
+class IntLiteral:
+    value: int
+    type: SimType=IntType
+    def __init__(self, *args):
+        self.value = int(*args)
+
+@dataclass 
+class FracLiteral:
+    value: Fraction
+    type: SimType=FracType
+    def __init__(self, *args):
+        self.value = Fraction(*args)
 
 @dataclass
 class BinOp:
     operator: str
     left: 'AST'
     right: 'AST'
-    type: Optional[SimType] = None
+    type: SimType = NumType | BoolType | StringType | IntType | FracType
 
 @dataclass
 class Variable:
@@ -52,14 +72,12 @@ class UnOp:
     operator: str
     vari : int
 
-
 @dataclass
 class StringOp:
     operator:str
     left:'AST'
     right:Optional['AST']=None
-    #type:StringLiteral
-    
+    # type:StringLiteral
 
 @dataclass
 class Let:
@@ -74,7 +92,11 @@ class IfElse:
     iffalse: 'AST'
     type: Optional[SimType] = None
 
-AST = NumLiteral | BoolLiteral | BinOp | IfElse | StringLiteral
+@dataclass
+class PrintOp:
+    inp: 'AST'
+
+AST = NumLiteral | BoolLiteral | BinOp | IfElse | StringLiteral | IntLiteral | FracLiteral
 
 
 TypedAST = NewType('TypedAST', AST)
@@ -91,15 +113,38 @@ def typecheck(program: AST, env = None) -> TypedAST:
             return t
         case StringLiteral() as t:
             return t
+        case IntLiteral() as t:
+            return t
+        case FracLiteral() as t:
+            return t
+    
         case BinOp(op, left, right) if op in ["+", "*"]:
             tleft = typecheck(left)
             tright = typecheck(right)
             
+            if tleft.type != tright.type :
+                print(f"left: {tleft}.type")
+                print(f"right: {tright}.type")
+                raise TypeError()
+            return BinOp(op, left, right,tleft)
+        
+        case BinOp(op, left, right) if op in ["/"]:
+            tleft = typecheck(left)
+            tright = typecheck(right)
             if tleft.type != NumType or tright.type != NumType:
                 print(f"left: {tleft}.type")
                 print(f"right: {tright}.type")
                 raise TypeError()
-            return BinOp(op, left, right, NumType)
+            if tleft.type != IntType or tright.type != IntType:
+                print(f"left: {tleft}.type")
+                print(f"right: {tright}.type")
+                raise TypeError()
+            elif tleft.type != FracType or tright.type != FracType:
+                print(f"left: {tleft}.type")
+                print(f"right: {tright}.type")
+                raise TypeError()
+            return BinOp(op, left, right)
+
         case BinOp("<", left, right):
             tleft = typecheck(left)
             tright = typecheck(right)
@@ -121,12 +166,17 @@ def typecheck(program: AST, env = None) -> TypedAST:
             if tt.type != tf.type: # Both branches must have the same type.
                 raise TypeError()
             return IfElse(tc, tt, tf, tt.type) # The common type becomes the type of the if-else.
+        case PrintOp(inp):
+            if inp==None:
+                raise TypeError()
     raise TypeError()
 Value = Fraction
 
 class InvalidProgram(Exception):
     pass
 
+
+    
 
 def eval(program: AST, environment: Mapping[str, Value] = None) -> Value:
     if environment is None:
@@ -136,6 +186,10 @@ def eval(program: AST, environment: Mapping[str, Value] = None) -> Value:
             return value
         case StringLiteral(value):
             return value
+        case IntLiteral(value):
+            return int(value)
+        case FracLiteral(value):
+            return Fraction(value)
         case Variable(name):
             if name in environment:
                 return environment[name]
@@ -144,38 +198,51 @@ def eval(program: AST, environment: Mapping[str, Value] = None) -> Value:
             v1 = eval(e1, environment)
             return eval(e2, environment | { name: v1 })
         case BinOp("+", left, right):
-            return eval(left, environment) + eval(right, environment)
+            left_type=typecheck(left).type
+            right_type=typecheck(right).type
+            if (left_type==NumType and right_type== NumType):
+                return eval(left, environment) + eval(right, environment)
+            elif(left_type==IntType and right_type== IntType):
+                return int(eval(left, environment) + eval(right, environment))
+            elif(left_type==FracType and right_type== FracType):
+                return Fraction(Fraction(eval(left, environment)) + Fraction(eval(right, environment)))
+            else:
+                # print(left_type)
+                # print(right_type)
+                raise TypeError()
+
         case BinOp("-", left, right):
             return eval(left, environment) - eval(right, environment)
         case BinOp("*", left, right):
-            return eval(left, environment) * eval(right, environment)
+            left_type=typecheck(left).type
+            right_type=typecheck(right).type
+            if (left_type==NumType and right_type== NumType):
+                return eval(left, environment) * eval(right, environment)
+            elif(left_type==IntType and right_type== IntType):
+                return int(eval(left, environment) * eval(right, environment))
+            elif(left_type==FracType and right_type== FracType):
+                return Fraction(Fraction(eval(left, environment)) * Fraction(eval(right, environment)))
+            else:
+                # print(left_type)
+                # print(right_type)
+                raise TypeError()
+
         case BinOp("/", left, right):
-            if(right==0):
-                raise InvalidProgram()
-            return eval(left, environment) / eval(right, environment)
-        case BinOp("//", left, right):
-            if(right==0):
-                raise InvalidProgram()
-            return eval(left, environment) // eval(right, environment)
-        case BinOp("%", left, right):
-            if(right==0):
-                raise InvalidProgram()
-            return eval(left, environment) % eval(right, environment)
-        case BinOp("**", left, right):
-            return eval(left, environment) ** eval(right, environment)
-        case BinOp("==",left,right):
-            return eval(left, environment) == eval(right, environment)
-        case BinOp("<",left,right):
-            return eval(left, environment) < eval(right, environment)
-        case BinOp(">",left,right):
-            return eval(left, environment) > eval(right, environment)
-        case BinOp(">=",left,right):
-            return eval(left, environment) >= eval(right, environment)
-        case BinOp("<=",left,right):
-            return eval(left, environment) <= eval(right, environment)
-        case BinOp("!=",left,right):
-            return eval(left, environment) != eval(right, environment)
-    
+            left_type=typecheck(left).type
+            right_type=typecheck(right).type
+            if (left_type==NumType and right_type== NumType):
+                return eval(left, environment) / eval(right, environment)
+            elif(left_type==IntType and right_type== IntType):
+                return int(eval(left, environment) / eval(right, environment))
+            elif(left_type==FracType and right_type== FracType):
+                return Fraction(Fraction(eval(left, environment)) / Fraction(eval(right, environment)))
+            else:
+                # print(left_type)
+                # print(right_type)
+                raise TypeError()
+
+            
+
         # Bitwise Operators With type checking
         case BinOp("&",left,right):
             left_type=typecheck(left).type
@@ -184,7 +251,7 @@ def eval(program: AST, environment: Mapping[str, Value] = None) -> Value:
             if(left_type!=NumType or right_type!=NumType):
                 # print(left_type)
                 # print(right_type)
-                raise InvalidProgram()
+                raise TypeError()
             return int(eval(left,environment)) & int(eval(right,environment))
         case BinOp("|",left,right):
             left_type=typecheck(left).type
@@ -193,16 +260,16 @@ def eval(program: AST, environment: Mapping[str, Value] = None) -> Value:
             if(left_type!=NumType or right_type!=NumType):
                 print(left_type)
                 print(right_type)
-                raise InvalidProgram()
+                raise TypeError()
             return int(eval(left,environment)) | int(eval(right,environment))
         case BinOp("^",left,right):
             left_type=typecheck(left).type
             right_type=typecheck(right).type
             
             if(left_type!=NumType or right_type!=NumType):
-                print(left_type) 
+                print(left_type)
                 print(right_type)
-                raise InvalidProgram()
+                raise TypeError()
             return int(eval(left,environment)) ^ int(eval(right,environment))
         case BinOp(">>",left,right):
             left_type=typecheck(left).type
@@ -211,7 +278,7 @@ def eval(program: AST, environment: Mapping[str, Value] = None) -> Value:
             if(left_type!=NumType or right_type!=NumType):
                 print(left_type)
                 print(right_type)
-                raise InvalidProgram()
+                raise TypeError()
             return int(eval(left,environment)) >> int(eval(right,environment))
         case BinOp("<<",left,right):
             left_type=typecheck(left).type
@@ -220,16 +287,9 @@ def eval(program: AST, environment: Mapping[str, Value] = None) -> Value:
             if(left_type!=NumType or right_type!=NumType):
                 print(left_type)
                 print(right_type)
-                raise InvalidProgram()
+                raise TypeError()
             return int(eval(left,environment)) << int(eval(right,environment))
-  
-        # String Operations
-        # implement string typecheck for this
-        case StringOp('add',left,right):
-            return eval(left)+eval(right)
-        case StringOp('length',left):
-            return len(eval(left))
-        
+
         #unary Operations
         case UnOp('-',vari):
             un=eval(vari)
@@ -244,86 +304,67 @@ def eval(program: AST, environment: Mapping[str, Value] = None) -> Value:
             un=un-1
             return eval(NumLiteral(un))
 
+        
+        # String Operations
+        # implement string typecheck for this
+        case StringOp('add',left,right):
+            return eval(left)+eval(right)
+        case StringOp('length',left):
+            return len(eval(left))
+        
+        #print Operation
+        case PrintOp(inp):
+            print(inp)
+            return inp
+        
     raise InvalidProgram()
 
+def test_division():
+    # a=FracLiteral(2)
+    # b=FracLiteral(4)
+    # c=BinOp("/",a,b)
+    # print(c)
+    # assert eval(c)== 1/2
 
-def test_typecheck():
-    import pytest
-    te = typecheck(BinOp("+", NumLiteral(2), NumLiteral(3)))
-    assert te.type == NumType
-    te = typecheck(BinOp("<", NumLiteral(2), NumLiteral(3)))
-    assert te.type == BoolType
-    # with pytest.raises(TypeError):
-    #     typecheck(BinOp("+", BinOp("*", NumLiteral(2), NumLiteral(3)), BinOp("<", NumLiteral(2), NumLiteral(3))))
-
-def test_string():
-    a=StringLiteral("hello ")
-    b=StringLiteral("world! ")
-    c=StringOp("add",a,b)
-    print(eval(c))
-    print(eval(StringOp('length',c)))
-    #print(eval(StringOp('slice',)))
-
-def test_unop():
+    a=IntLiteral(5)
+    print(type(a))
+    b=IntLiteral(2)
+    print(type(b))
+    c=BinOp("+",a,b)
+    print(type(eval(c)))
     
-    a=Variable("a")
-    e1=NumLiteral(1) 
-    e2=UnOp("++",e1)
-    e3=UnOp("-",e1)
-    e4=UnOp("--",e1)
-    assert eval(e3)== -1
-    assert eval(e2)==2
-    assert eval(e4)== 0
+    # d=IntLiteral(9)
+    # print(type(d))
+    # c=BinOp("/",BinOp("+",a,d),b)
+    # print(type(c))
+    # print(eval(c))
+    # assert eval(c)== 7
 
-    a=Variable("a")
-    e1=NumLiteral(2)
-    b=NumLiteral(1)
-    e2=BinOp("+",b,BinOp("*",UnOp("--",e1),e1))     #1+((2--)*2) = 3
-    assert eval(e2)==3
+    # a=NumLiteral(5)                 #Using Numliteral We can get float values
+    # b=NumLiteral(2)
+    # c=BinOp("/",a,b)
+    # print(c)
+    # assert eval(c)== 2.5
+
+    # a=IntLiteral(5)               #//If the opreands hav different type Like Int and Fraction 
+    # b=FracLiteral(2)
+    # c=BinOp("/",a,b)
+    # print(c)
+    # assert eval(c)== 2.5
+
+test_division()
+
+
+
+
+
+
+
+
+
+
+
+
     
-    
-    e1=NumLiteral(10)
-    b=NumLiteral(5)
-    c=NumLiteral(2)
-    e2=BinOp("+",UnOp("++",e1),BinOp("*",UnOp("-",c),b))     #(10++) + ((-2)*5) = 1
-    assert eval(e2)==1
 
-    a  = Variable("a")
-    e1 = NumLiteral(5)
-    e2 = BinOp("+", a, a)
-    e3 = UnOp("++",Let(a, e1, BinOp("+", a, Let(a, e2, e2))))
-    assert eval(e3)==26
-
-def test_bit():
-    # "Bitwise AND"
-    a=NumLiteral(15)
-    b=NumLiteral(4)
-    c=BinOp("&",a,b)   
-    assert eval(c)==4
-
-    # "Bitwise OR"
-    a=NumLiteral(12)
-    b=NumLiteral(20)
-    c=BinOp("|",a,b)
-    assert eval(c)==28
-
-    # "Bitwise XOR"
-    a=NumLiteral(34)
-    b=NumLiteral(41)
-    c=BinOp("^",a,b)
-    assert eval(c)==11
-
-def test_ls_rs():
-    
-    # "Bitwise Right Shift"
-    a=NumLiteral(10)
-    b=NumLiteral(1)
-    c=BinOp(">>",a,b)
-    assert eval(c)==5
-
-    # "Bitwise Left Shift"
-    a=NumLiteral(8)
-    b=NumLiteral(3)
-    c=BinOp("<<",a,b)
-    assert eval(c)==64
 
