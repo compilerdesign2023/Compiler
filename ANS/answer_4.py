14eaf4fa2114c2a1f11942238085d517a6b37d40
Aditya Shukla <95519091+adityashukla700@users.noreply.github.com>
2023-04-24 15:58:46 +0530

    Create answer_4.py

---
+++ ANS/answer_4.py
@@ -0,0 +1,13 @@
+let s=String "this is a string" in
+let l=s.length in
+let n=0 in
+let i=1 in
+{
+    for i<l-1 up i+=1 do
+        let next=i+1 in
+        let c=s.slice i next 1 in
+        let e=String " " in
+        if c==e then {
+            n=n+1;
+        }; end;
+} end
